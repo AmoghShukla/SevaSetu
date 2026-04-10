ce38cfa9349bd4b04084d3a5ed72e954a973a871
Amogh Shukla <amoghshukla548@gmail.com>
2026-04-10 22:35:56 +0530

    Load environment variables and update database connection setup in session.py

--- database/session.py
+++ database/session.py
@@ -1,8 +1,13 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-create_engine()
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
 
 Sessionlocal = sessionmaker(bind=engine)
 
