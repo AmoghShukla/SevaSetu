66517ca27459ba30967aa4e58c6a52eca25d6896
Amogh Shukla <amoghshukla548@gmail.com>
2026-04-10 22:33:52 +0530

    Fix session.py to implement database session management and define get_db function

--- database/session.py
+++ database/session.py
@@ -1,2 +1,13 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+
+create_engine()
+
+Sessionlocal = sessionmaker(bind=engine)
+
+def get_db():
+    db = Sess
+    try:
+        yield db
+    finally:
+        db.
