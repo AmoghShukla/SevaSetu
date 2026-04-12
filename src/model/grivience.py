4c38f9a874083d81b2ff12162d05253e5398aa92
Amogh Shukla <amoghshukla548@gmail.com>
2026-04-12 20:37:43 +0530

    feat: define Grivience_Class model and update User_Class attributes in user.py

---
+++ src/model/grivience.py
@@ -0,0 +1,13 @@
+from src.database.base import Base
+from sqlalchemy import column, Integer, String, ForeignKey, Text
+from sqlalchemy.orm import relationship
+
+class Grivience_Class(Base):
+    __tablename__="Grivience"
+
+    grivience_id = column(Integer, primary_key=True, index=True)
+    grivience_title = column(String, nullable=False)
+    grivience_description = column(Text, nullable=False)
+
+    user_id = column(Integer, ForeignKey("User"))
+    grivience_status = column(...,nullable=False)
--- src/model/user.py
+++ src/model/user.py
@@ -1,14 +1,15 @@
 from src.database.base import Base
-from sqlalchemy import column
+from sqlalchemy import column, Integer, String, ForeignKey, Text
+from sqlalchemy.orm import relationship
 
 class User_Class(Base):
     __tablename__="User"
 
-    user_id = column(...,nullable=False)
-    user_name = column(...,nullable=False)
-    user_email = column(..., nullable=False)
-    user_password = column(...,nullable=False)
-    user_contact = column(...,nullable=False)
-    user_role = column(...,nullable=False)
-    user_pincode = column(...,nullable=False)
-    user_department_id = column(..., nullable=True)
+    user_id = column(Integer, primary_key=True,index=True)
+    user_name = column(String, nullable=False)
+    user_email = column(String, nullable=False)
+    user_password = column(String, nullable=False)
+    user_contact = column(String, nullable=False)
+    user_role = column(String, nullable=False)
+    user_pincode = column(Integer, nullable=False)
+    user_department_id = column(Integer, nullable=False)
